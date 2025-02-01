1b7a2657856a0cadaf700d5159457488081c89d2
parthmca007 <parth.mudaliar@gmail.com>
2025-02-01 12:26:41 +0530

    imported cors

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,7 @@ const cors_1 = __importDefault(require("cors"));
 const db_1 = require("./config/db");
 const sequelize_1 = require("sequelize"); // Import QueryTypes
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors = require('cors');
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
