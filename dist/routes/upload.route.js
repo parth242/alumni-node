51211a56e0cb282bac4b581787034b5f1900b9b3
parthmca007 <parth.mudaliar@gmail.com>
2025-03-27 11:11:29 +0530

    upload route

--- dist/routes/upload.route.js
+++ dist/routes/upload.route.js
@@ -36,48 +36,52 @@ const route = express_1.default.Router();
  *       500:
  *         description: Internal server error
  */
+route.get("/deleteOldImage", async (req, res) => {
+    // initializeStateModel(getSequelize());
+    try {
+        const key = req.query?.key?.toString() || "";
+        if (key != '') {
+            // Start Tebi Cloud
+            const credentials = {
+                accessKeyId: process.env.TEBI_S3_ACCESS_KEY_ID || "",
+                secretAccessKey: process.env.TEBI_S3_SECRET_ACCESS_KEY || "",
+            };
+            console.log("credentials", credentials);
+            // Create an S3 service client object.
+            const s3Client = new client_s3_1.S3Client({
+                endpoint: process.env.TEBI_S3_ENDPOINT_URL,
+                credentials: credentials,
+                region: process.env.TEBI_S3_REGION,
+            });
+            console.log("process.env.TEBI_S3_ENDPOINT_URL", process.env.TEBI_S3_ENDPOINT_URL);
+            // Generate a presigned URL
+            const get_command = new client_s3_1.DeleteObjectCommand({
+                Bucket: process.env.TEBI_S3_BUCKET_NAME,
+                Key: key,
+                // ResponseContentDisposition: 'attachment; filename="' + file + '"'
+            });
+            await s3Client.send(get_command);
+            res.status(200).json({
+                message: "image deleted",
+                status: 200,
+            });
+        }
+        else {
+            res.status(500).json({ message: 'No image found for delete' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: (0, functions_1.catchError)(error) });
+    }
+});
 route.get("/", async (req, res) => {
     // initializeStateModel(getSequelize());
     try {
-        /* if(req.query.hasOwnProperty('page_size')){
-            pageSize = req.query.page_size; // Page size
-        } else{
-            pageSize = 10; // Page size
-        } */
-        /* req.body.sessionUser.id = 1;
-        const user = await Users.findOne({
-            where: { id: req.body.sessionUser.id },
-        });
-        if (!user) {
-            res.status(500).json({ message: "Invalid User" });
-            return;
-        } */
         const type = req.query?.type || "general";
         console.log("req.query?.type", req.query?.type);
         const fileName = req.query?.filename?.toString() || "";
         const fileExt = fileName.split(".").pop() || "jpeg";
         const key = `${type}/${(0, uuid_1.v4)()}.` + fileExt;
-        /* if (process.env.NEXT_PUBLIC_IS_S3_BUCKET === "true") {
-        const s3 = new AWS.S3({
-            credentials: {
-                accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
-                secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
-            },
-            region: process.env.AWS_REGION as string,
-        });
-
-        s3.getSignedUrl(
-            'putObject',
-            {
-                Bucket: process.env.AWS_S3_BUCKET_NAME as string,
-                ContentType: 'image/jpeg',
-                Key: key,
-            },
-            (err, url) => {
-                return NextResponse.json({ data: { key, url } });
-            }
-        );
-    } else { */
         // Start Tebi Cloud
         const credentials = {
             accessKeyId: process.env.TEBI_S3_ACCESS_KEY_ID || "",
--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { initializeStateModel } from "../models/State";
 import { getSequelize } from "../config/db";
 import { catchError } from "../common/functions";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuidv4 } from "uuid";
 import { auth } from "../middleware/auth";
@@ -37,23 +37,56 @@ const route = express.Router();
  *       500:
  *         description: Internal server error
  */
+
+route.get("/deleteOldImage", async (req, res) => {
+	// initializeStateModel(getSequelize());
+	try {
+		
+	
+		const key = req.query?.key?.toString() || "";
+
+		if(key!=''){
+		// Start Tebi Cloud
+		const credentials = {
+			accessKeyId: process.env.TEBI_S3_ACCESS_KEY_ID || "",
+			secretAccessKey: process.env.TEBI_S3_SECRET_ACCESS_KEY || "",
+		};
+		console.log("credentials", credentials);
+		// Create an S3 service client object.
+		const s3Client = new S3Client({
+			endpoint: process.env.TEBI_S3_ENDPOINT_URL,
+			credentials: credentials,
+			region: process.env.TEBI_S3_REGION,
+		});
+		console.log(
+			"process.env.TEBI_S3_ENDPOINT_URL",
+			process.env.TEBI_S3_ENDPOINT_URL,
+		);
+		// Generate a presigned URL
+		const get_command = new DeleteObjectCommand({
+			Bucket: process.env.TEBI_S3_BUCKET_NAME,
+			Key: key,
+			// ResponseContentDisposition: 'attachment; filename="' + file + '"'
+		});
+		await s3Client.send(get_command);
+		
+
+		res.status(200).json({
+			message: "image deleted",
+			status: 200,			
+		});
+	} else{
+		res.status(500).json({ message: 'No image found for delete' });
+	}
+	} catch (error) {
+		res.status(500).json({ message: catchError(error) });
+	}
+});
+
 route.get("/", async (req, res) => {
 	// initializeStateModel(getSequelize());
 	try {
-		/* if(req.query.hasOwnProperty('page_size')){
-			pageSize = req.query.page_size; // Page size
-		} else{
-			pageSize = 10; // Page size
-		} */
-		/* req.body.sessionUser.id = 1;
-		const user = await Users.findOne({
-			where: { id: req.body.sessionUser.id },
-		});
-		if (!user) {
-			res.status(500).json({ message: "Invalid User" });
-			return;
-		} */
-
+		
 		const type = req.query?.type || "general";
 		console.log("req.query?.type", req.query?.type);
 		const fileName = req.query?.filename?.toString() || "";
@@ -61,27 +94,7 @@ route.get("/", async (req, res) => {
 
 		const key = `${type}/${uuidv4()}.` + fileExt;
 
-		/* if (process.env.NEXT_PUBLIC_IS_S3_BUCKET === "true") {
-        const s3 = new AWS.S3({
-            credentials: {
-                accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
-                secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
-            },
-            region: process.env.AWS_REGION as string,
-        });
-
-        s3.getSignedUrl(
-            'putObject',
-            {
-                Bucket: process.env.AWS_S3_BUCKET_NAME as string,
-                ContentType: 'image/jpeg',
-                Key: key,
-            },
-            (err, url) => {
-                return NextResponse.json({ data: { key, url } });
-            }
-        );
-    } else { */
+		
 		// Start Tebi Cloud
 		const credentials = {
 			accessKeyId: process.env.TEBI_S3_ACCESS_KEY_ID || "",
@@ -117,5 +130,6 @@ route.get("/", async (req, res) => {
 		res.status(500).json({ message: catchError(error) });
 	}
 });
+
 
 export default route;
